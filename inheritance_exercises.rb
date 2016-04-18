fbe09b120c4bfa2df6321c0d7b3a2080d1b333fc
App Academy Student <appacademy@Apps-Mac-mini-60.local>
2016-04-18 13:58:15 -0400

    finished inheritance exercise 1

--- inheritance_exercises.rb
+++ inheritance_exercises.rb
@@ -4,18 +4,51 @@ class Employee
     @title = title
     @salary = salary
     @boss = boss
+
+    work_under_manager unless boss.nil?
   end
 
   def bonus(multiplier)
+    @salary * multiplier
+  end
+
+  protected
+  def report_salary
+    @salary
+  end
+
+  private
+  def work_under_manager
+    @boss.add_employee(self)
   end
 end
 
 class Manager < Employee
-  def initialize
-    # override
+  def initialize(name, title, salary, boss, employees = [])
+    super(name, title, salary, boss)
+    @employees = employees
   end
 
   def bonus(multiplier)
-    # override
+    salary_of_each_employee * multiplier
+  end
+
+  def add_employee(employee)
+    @employees << employee
+  end
+
+  protected
+  def salary_of_each_employee
+    combined_salary = 0
+
+    @employees.each do |underling|
+      if underling.is_a?(Manager)
+        combined_salary += underling.salary_of_each_employee + underling.report_salary
+      else
+        combined_salary += underling.report_salary
+      end
+    end
+
+    combined_salary
   end
 end
